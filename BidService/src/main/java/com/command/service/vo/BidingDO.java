d53459234845d63be46a79259969eb60389516ef
debarshicroy <debarshichandaroy@gmail.com>
2022-08-08 12:06:04 +0530

    Committing BidingDO changes

--- BidService/src/main/java/com/command/service/vo/BidingDO.java
+++ BidService/src/main/java/com/command/service/vo/BidingDO.java
@@ -20,6 +20,16 @@ public class BidingDO {
 	private String bidderName;
 	@JsonProperty("bidAmount")
 	private double bidAmount;
+	@JsonProperty("bidderEmailId")
+	private String bidderEmailId;
+	@JsonProperty("bidderPhNo")
+	private String bidderPhNo;
+	@JsonProperty("bidderAdd")
+	private String bidderAdd;
+	@JsonProperty("bidderPincode")
+	private String bidderPincode;
+	@JsonProperty("bidderState")
+	private String bidderState;
 	
 	public String getName() {
 		return name;
@@ -39,20 +49,57 @@ public class BidingDO {
 	public void setBidAmount(double bidAmount) {
 		this.bidAmount = bidAmount;
 	}
-	@Override
-	public String toString() {
-		return "BidingDO [id=" + id + ", productName=" + name + ", bidderName=" + bidderName + ", bidAmount="
-				+ bidAmount + "]";
+	public String getBidderEmailId() {
+		return bidderEmailId;
 	}
-	public BidingDO(String productName, String bidderName, double bidAmount) {
+	public void setBidderEmailId(String bidderEmailId) {
+		this.bidderEmailId = bidderEmailId;
+	}
+	public String getBidderPhNo() {
+		return bidderPhNo;
+	}
+	public void setBidderPhNo(String bidderPhNo) {
+		this.bidderPhNo = bidderPhNo;
+	}
+	public String getBidderAdd() {
+		return bidderAdd;
+	}
+	public void setBidderAdd(String bidderAdd) {
+		this.bidderAdd = bidderAdd;
+	}
+	public String getBidderPincode() {
+		return bidderPincode;
+	}
+	public void setBidderPincode(String bidderPincode) {
+		this.bidderPincode = bidderPincode;
+	}
+	public String getBidderState() {
+		return bidderState;
+	}
+	public void setBidderState(String bidderState) {
+		this.bidderState = bidderState;
+	}
+	public BidingDO(int id, String name, String bidderName, double bidAmount, String bidderEmailId, String bidderPhNo,
+			String bidderAdd, String bidderPincode, String bidderState) {
 		super();
-		this.name = productName;
+		this.id = id;
+		this.name = name;
 		this.bidderName = bidderName;
 		this.bidAmount = bidAmount;
+		this.bidderEmailId = bidderEmailId;
+		this.bidderPhNo = bidderPhNo;
+		this.bidderAdd = bidderAdd;
+		this.bidderPincode = bidderPincode;
+		this.bidderState = bidderState;
 	}
 	public BidingDO() {
 		super();
+		// TODO Auto-generated constructor stub
 	}
-	
-	
+	@Override
+	public String toString() {
+		return "BidingDO [id=" + id + ", name=" + name + ", bidderName=" + bidderName + ", bidAmount=" + bidAmount
+				+ ", bidderEmailId=" + bidderEmailId + ", bidderPhNo=" + bidderPhNo + ", bidderAdd=" + bidderAdd
+				+ ", bidderPincode=" + bidderPincode + ", bidderState=" + bidderState + "]";
+	}	
 }
